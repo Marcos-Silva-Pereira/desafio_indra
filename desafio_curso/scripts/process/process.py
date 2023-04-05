0e7eee4363920630bec05d2136b31fff27c220d4
Marcos-Silva-Pereira <marcos.pereira_mcs@hotmail.com>
2023-04-05 17:51:12 -0300

    v5

--- desafio_curso/scripts/process/process.py
+++ desafio_curso/scripts/process/process.py
@@ -19,6 +19,81 @@ df_regiao = spark.sql("SELECT * FROM DESAFIO_CURSO.TBL_REGIAO")
 df_vendas = spark.sql("SELECT * FROM DESAFIO_CURSO.TBL_VENDAS")
 
 # Espaço para tratar e juntar os campos e a criação do modelo dimensional
+# Tratando df_endereco
+# retirando a primeira linha, pois repete o nome das colunas
+df_endereco = df_endereco.where(df_endereco.city != 'City')
+
+# mudando o tipo das colunas
+li = ["address_number", "zip_code"]
+for x in li:
+    df_endereco = df_endereco.withColumn(x, df_endereco[x].cast(IntegerType()))
+
+# substituindo valores nulos e vazios por 0, e strings vazias por 'Não informado'
+for z in df_endereco.dtypes:
+    if re.search("int", z[1]) :
+        df_endereco = df_endereco.withColumn(z[0], when(df_endereco[z[0]].isNull(), 0)\
+                                            .otherwise(df_endereco[z[0]]))
+    if re.search("double", z[1]):
+        df_endereco = df_endereco.withColumn(z[0], when(df_endereco[z[0]].isNull(), 0.0)\
+                                            .otherwise(df_endereco[z[0]]))
+    else:
+        df_endereco = df_endereco.withColumn(z[0], trim(df_endereco[z[0]]))
+        df_endereco = df_endereco.withColumn(z[0], when(df_endereco[z[0]] == '', 'Não informado')\
+                                            .when(df_endereco[z[0]].isNull(), 'Não informado')  
+                                            .otherwise(df_endereco[z[0]]))
+
+# Tratando df_clientes
+# retirando a primeira linha, pois repete o nome das colunas
+df_clientes = df_clientes.where(df_clientes.address_number != 'Address Number')
+
+# mudando o tipo das colunas
+li = ["address_number", "business_unit", "customer_key", "division", "region_code"]
+for x in li:
+    df_clientes = df_clientes.withColumn(x, df_clientes[x].cast(IntegerType()))
+
+# substituindo valores nulos e vazios por 0, e strings vazias por 'Não informado'
+for z in df_clientes.dtypes:
+    if re.search("int", z[1]) :
+        df_clientes = df_clientes.withColumn(z[0], when(df_clientes[z[0]].isNull(), 0)\
+                                            .otherwise(df_clientes[z[0]]))
+    if re.search("double", z[1]):
+        df_clientes = df_clientes.withColumn(z[0], when(df_clientes[z[0]].isNull(), 0.0)\
+                                            .otherwise(df_clientes[z[0]]))
+    else:
+        df_clientes = df_clientes.withColumn(z[0], trim(df_clientes[z[0]]))
+        df_clientes = df_clientes.withColumn(z[0], when(df_clientes[z[0]] == '', 'Não informado')\
+                                            .when(df_clientes[z[0]].isNull(), 'Não informado')  
+                                            .otherwise(df_clientes[z[0]]))
+
+# Tratando df_vendas
+# retirando a primeira linha, pois repete o nome das colunas
+df_vendas = df_vendas.where(df_vendas.actual_delivery_date != 'Actual Delivery Date')
+
+# mudando o tipo das colunas
+# inteiros
+li = ["customer_key", "invoice_number", "item_number", "line_number", "order_number", "sales_quantity", "sales_rep"]
+for x in li:
+    df_vendas = df_vendas.withColumn(x, df_vendas[x].cast(IntegerType()))
+    
+# double
+lf = ["discount_amount", "list_price", "sales_amount", "sales_amount_based_on_list_price", "sales_cost_amount", "sales_margin_amount", "sales_price"]
+for y in lf:
+    df_vendas = df_vendas.withColumn(y, df_vendas[y].cast(DoubleType()))
+
+# substituindo valores nulos e vazios por 0, e strings vazias por 'Não informado'
+for z in df_vendas.dtypes:
+    if re.search("int", z[1]) :
+        df_vendas = df_vendas.withColumn(z[0], when(df_vendas[z[0]].isNull(), 0)\
+                                            .otherwise(df_vendas[z[0]]))
+    if re.search("double", z[1]):
+        df_vendas = df_vendas.withColumn(z[0], when(df_vendas[z[0]].isNull(), 0.0)\
+                                            .otherwise(df_vendas[z[0]]))
+    else:
+        df_vendas = df_vendas.withColumn(z[0], trim(df_vendas[z[0]]))
+        df_vendas = df_vendas.withColumn(z[0], when(df_vendas[z[0]] == '', 'Não informado')\
+                                            .when(df_vendas[z[0]].isNull(), 'Não informado')  
+                                            .otherwise(df_vendas[z[0]]))
+
 
 # criando o fato
 ft_vendas = []
